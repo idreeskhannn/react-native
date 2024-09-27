22c5cae22ba25dba65e4ec1d4e86d009db0094db
idreeskhan42 <midreeskhan7672@gmail.com>
2024-09-28 01:30:38 +0500

    commit

---
+++ app.js
@@ -0,0 +1,142 @@
+// alert("hello world")
+
+////////   QUESTION = 1   ///////
+
+// let num1 = +prompt("enter a number " + " 1 ");
+// let num2 = +prompt("enter a number " + " 2 ");
+
+// if (num1 > num2) {
+//     console.log(num1)
+// }else if (num2 > num1) {
+//     console.log(num2)
+// }
+// else if (num1 === num2) {
+//     console.log("both numbers  equal")
+// }else {
+//     console.log("enter a number")
+// };
+
+
+
+
+//////////////   QUESTION = 2   ////////////
+
+
+
+//  let sign = +prompt("enter a number");
+
+//  if(sign < 0){
+//     console.log("sign is -")
+//  }
+//  else if(sign >0){
+// console.log("sign is +")
+//  }
+//  else{console.log("enter a number")};
+
+
+
+
+//////////////   QUESTION = 3   ////////////
+
+
+
+
+//  let num1 = +prompt("enter a number " + " 1 ");
+// let num2 = +prompt("enter a number " + " 2 ");
+// let num3 = +prompt("enter a number " + " 3 ");
+// let num4 = +prompt("enter a number " + " 4 ");
+// let num5 = +prompt("enter a number " + " 5 ");
+
+
+// if (num1 > num2 && num1 >num3 && num1 >num4 && num1 >num5 && num1===num1) {
+//     console.log(num1)
+// }
+// if (num2 > num1 && num2 >num3 && num2 >num4 && num2 >num5 && num2===num2) {
+//     console.log(num2)
+// }if (num3 > num1 && num3 >num2 && num3 >num4 && num3 >num5 && num3===num3) {
+//     console.log(num3)
+// }if (num4 > num1 && num4 >num2 && num4 >num3 && num4 >num5 && num4===num4) {
+//     console.log(num4)
+// }if (num5 > num1 && num5 >num2 && num5 >num3 && num5 >num4 && num5===num5) {
+//     console.log(num5)
+// }
+// else{
+//     console.log("enter a number")
+// }
+
+
+
+
+
+/////////////   QUESTION = 4  ////////////
+
+
+
+//   for (let i =0 ; i<=15 ; i++){
+//     if(i % 2 === 0){
+//         console.log(i  +" even")
+//     }
+//     else if(!i % 2 === 0){
+//         console.log(i +" odd")
+//     }
+//   };
+
+
+
+
+/////////////   QUESTiON = 5  ////////////
+
+
+
+// let grade = +prompt("enter percentage");
+
+// if (grade <60) {
+//     console.log("you are fail")
+// }
+// else if (grade <70) {
+//     console.log("D grade")
+// }
+// else if (grade <80) {
+//     console.log("C grade")
+// }
+// else if (grade <90) {
+//     console.log("B grade")
+// }
+// else if (grade <=100) {
+//     console.log("A grade")
+// }
+// else{
+//     console.log("enter actual percentage")
+// }
+
+
+/////////////   QUESTION = 6  ////////////
+
+// let num = 100
+// for (let i =0 ; i<=100 ; i++){
+//     if(i%3=== 0 && i%5===0){
+//         console.log(i+"fizzbuzz")
+//     }
+//    else if(i%3===0  ){
+// console.log(i+ "fizz")
+//     }
+//     else if(i%5===0){
+//         console.log(i+ "buzzz")
+
+//     }
+
+// }
+
+/////////////   QUESTION = 6  ////////////
+
+// let star="";
+// for (let i = 1; i<4; i++) {
+
+    
+//     for(let j=0 ;j<i; j++){
+        
+//         star +="*"
+//     }
+//     star += "\n";
+// }
+// console.log(star)
